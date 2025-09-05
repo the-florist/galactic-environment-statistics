b5caa7d538a5dce120e90c22b43a598870d05519
Ericka Florio <eaf49@cam.ac.uk>
2025-09-05 14:19:39 +0300

    Fix bug and add case 1 test
    A. Fix error in definition of D(a), where I forgot the factor of 1/a^(3/2) in the multiplicative factor.
    
    B. Add case 1 comparison (Omega_m = 1, Omega_L = 0):
        1. In this case, D(a) ~ a.
        2. Calulate the matter-only model D(a), reguardless of base values of Om and Ol.
        3. Estimate the slope from the first and last point in a = (0, 1).
        4. Plot the linear ''fit'' and matter-only model together.

--- growth-factor.py
+++ growth-factor.py
@@ -6,14 +6,14 @@ import scipy.integrate as integrate
 import parameters as mp
 
 # Define functions
-def integrand(x):
-    return pow(x / (x * (1 - mp.Omega_m - mp.Omega_L) + mp.Omega_m + mp.Omega_L * (x ** 3)), 3/2)
+def integrand(x, Om = mp.Omega_m, Ol = mp.Omega_L):
+    return pow(x / (x * (1 - Om - Ol) + Om + Ol * (x ** 3)), 3/2)
 
-def D(a, floor, return_full = False):
+def D(a, floor, return_full = False, Om = mp.Omega_m, Ol = mp.Omega_L):
     out_full = integrate.quad(lambda x: integrand(x), floor, a)
 
     D_temp = out_full[0]
-    D_temp *= np.sqrt(a * (1 - mp.Omega_m - mp.Omega_L) + mp.Omega_m + mp.Omega_L * (a ** 3))
+    D_temp *= np.sqrt(a * (1 - Om - Ol) + Om + Ol * (a ** 3)) / pow(a, 3/2)
     err = out_full[1]
     
     if(return_full):
@@ -25,15 +25,14 @@ def D(a, floor, return_full = False):
 out = D(mp.a_f, mp.a_i, return_full = True)
 D_i = out[0]
 err = out[1]
-
 print("D(a) = "+str(D_i))
 print("D(a) error: "+str(err))
 
+a_vals = np.linspace(mp.a_i, 1, mp.num_steps)
+D_vals = [D(a, mp.a_i) for a in a_vals]
+
 # Plot D(a)
 if(mp.print_D_a == True):
-    a_vals = np.linspace(mp.a_i, 1, mp.num_steps)
-    D_vals = [D(a, mp.a_i) for a in a_vals]
-
     plt.plot(a_vals, D_vals, label="D(a)")
     plt.xlabel("a")
     plt.ylabel("D(a)")
@@ -42,5 +41,17 @@ if(mp.print_D_a == True):
     plt.grid(True)
     plt.savefig("D-plot.pdf")
 
+if(mp.compare_case_1 == True):
+    matter_model = [D(a, mp.a_i, Om = 1, Ol = 0) for a in a_vals]
+    slope_est = (matter_model[-1] - matter_model[0])/(mp.a_f - mp.a_i)
+    
+    plt.plot(a_vals, matter_model, label="D(a), Omega_m = 1")
+    plt.plot(a_vals, slope_est * a_vals, label="Linear in a", linestyle='--')
+    plt.xlabel("a")
+    plt.ylabel("D(a)")
+    plt.title("Growth Factor D(a) vs Scale Factor a")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("D-plot-compare-case-1.pdf")
 
 print("Program finished.")
--- parameters.py
+++ parameters.py
@@ -1,10 +1,10 @@
-Omega_m = 0
-Omega_L = 1
+Omega_m = 1
+Omega_L = 0
 a_f = 1
 a_i = 0.01
 num_steps = 200
 
-print_D_a = True
+print_D_a = False
 
-compare_case_1 = False
+compare_case_1 = True
 compare_case_2 = False
