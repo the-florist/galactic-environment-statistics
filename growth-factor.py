05aa2900418503e896c831999e6b65d969cfabf4
Ericka Florio <eaf49@cam.ac.uk>
2025-09-05 13:01:18 +0300

    Base both D computations on D(a) and add comments

--- growth-factor.py
+++ growth-factor.py
@@ -2,31 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
+# Set parameters
 Omega_m = float(input("Enter Omega_m (0, 1): "))
 Omega_L = float(input("Enter Omega_L (0, 1): "))
 a_i = float(input("Enter a scale factor to evaluate at: "))
+lowest_a = 0.01
 
+# Define functions
 def integrand(x):
     return pow(x / (x * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (x ** 3)), 3/2)
 
-out = integrate.quad(lambda x: integrand(x), 0, a_i)
+def D(a, floor, return_full = False):
+    out_full = integrate.quad(lambda x: integrand(x), floor, a)
+
+    D_temp = out_full[0]
+    D_temp *= np.sqrt(a * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a ** 3))
+    err = out_full[1]
+    
+    if(return_full):
+        return [D_temp, err]
+    else:
+        return D_temp
+
+# Compute D(a) for a single value
+out = D(a_i, lowest_a, return_full = True)
 D_i = out[0]
 err = out[1]
-
-D_i *= np.sqrt(a_i * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a_i ** 3)) / pow(a_i, 3/2)
 
 print("D(a) = "+str(D_i))
 print("D(a) error: "+str(err))
 
+# Plot D(a)
 plot_factor = bool(input("Plot D(a) from (0, 1)? "))
-
 if(plot_factor):
-    def D(a, floor):
-        out_full = integrate.quad(lambda x: integrand(x), floor, a)[0] 
-        out_full *= np.sqrt(a * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a ** 3))
-        return out_full
-
-    lowest_a = 0.01
     a_vals = np.linspace(lowest_a, 1, 200)
     D_vals = [D(a, lowest_a) for a in a_vals]
 
