961446cf1854093a56842e0aea5eb771f2c4d5bf
Ericka Florio <eaf49@cam.ac.uk>
2025-09-05 12:54:07 +0300

    Add option to plot D(a)
    1. Ask user if plot of D(a) is required.
    2. If required, define D(a), compute D(a) for a range of values, plot that
    range and save to a PDF called "D-plot.pdf" in home folder.

--- growth-factor.py
+++ growth-factor.py
@@ -1,22 +1,42 @@
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import scipy.integrate as integrate
-import sys
 
 Omega_m = float(input("Enter Omega_m (0, 1): "))
 Omega_L = float(input("Enter Omega_L (0, 1): "))
 a_i = float(input("Enter a scale factor to evaluate at: "))
 
-def integrand(x, O_m, O_l):
-    return pow(x / (x * (1 - O_m - O_l) + O_m + O_l * (x ** 3)), 3/2)
+def integrand(x):
+    return pow(x / (x * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (x ** 3)), 3/2)
 
-out = integrate.quad(lambda x: integrand(x, Omega_m, Omega_L), 0, a_i)
-D = out[0]
+out = integrate.quad(lambda x: integrand(x), 0, a_i)
+D_i = out[0]
 err = out[1]
 
-D *= np.sqrt(a_i * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a_i ** 3)) / pow(a_i, 3/2)
+D_i *= np.sqrt(a_i * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a_i ** 3)) / pow(a_i, 3/2)
 
-print("D(a) = "+str(D))
+print("D(a) = "+str(D_i))
 print("D(a) error: "+str(err))
+
+plot_factor = bool(input("Plot D(a) from (0, 1)? "))
+
+if(plot_factor):
+    def D(a, floor):
+        out_full = integrate.quad(lambda x: integrand(x), floor, a)[0] 
+        out_full *= np.sqrt(a * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a ** 3))
+        return out_full
+
+    lowest_a = 0.01
+    a_vals = np.linspace(lowest_a, 1, 200)
+    D_vals = [D(a, lowest_a) for a in a_vals]
+
+    plt.plot(a_vals, D_vals, label="D(a)")
+    plt.xlabel("a")
+    plt.ylabel("D(a)")
+    plt.title("Growth Factor D(a) vs Scale Factor a")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("D-plot.pdf")
+
 
 print("Program finished.")
