fc146d17369b892b35d4ef12f3c16441d6147767
Ericka Florio <eaf49@cam.ac.uk>
2025-09-05 13:38:38 +0300

    Add parameters file
    1. Add parameters.py which separates parameters from the main file.
    2. Import parameters as mp (my parameters) and uses those.
    3. Confirmed I got D(a) = -4999 ish with 10^-9 error with Omega_m = 0, Omega_L = 1 at a_f = 1.

--- growth-factor.py
+++ growth-factor.py
@@ -2,21 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
-# Set parameters
-Omega_m = float(input("Enter Omega_m (0, 1): "))
-Omega_L = float(input("Enter Omega_L (0, 1): "))
-a_i = float(input("Enter a scale factor to evaluate at: "))
-lowest_a = 0.01
+# Import parameters
+import parameters as mp
 
 # Define functions
 def integrand(x):
-    return pow(x / (x * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (x ** 3)), 3/2)
+    return pow(x / (x * (1 - mp.Omega_m - mp.Omega_L) + mp.Omega_m + mp.Omega_L * (x ** 3)), 3/2)
 
 def D(a, floor, return_full = False):
     out_full = integrate.quad(lambda x: integrand(x), floor, a)
 
     D_temp = out_full[0]
-    D_temp *= np.sqrt(a * (1 - Omega_m - Omega_L) + Omega_m + Omega_L * (a ** 3))
+    D_temp *= np.sqrt(a * (1 - mp.Omega_m - mp.Omega_L) + mp.Omega_m + mp.Omega_L * (a ** 3))
     err = out_full[1]
     
     if(return_full):
@@ -25,7 +22,7 @@ def D(a, floor, return_full = False):
         return D_temp
 
 # Compute D(a) for a single value
-out = D(a_i, lowest_a, return_full = True)
+out = D(mp.a_f, mp.a_i, return_full = True)
 D_i = out[0]
 err = out[1]
 
@@ -33,10 +30,9 @@ print("D(a) = "+str(D_i))
 print("D(a) error: "+str(err))
 
 # Plot D(a)
-plot_factor = bool(input("Plot D(a) from (0, 1)? "))
-if(plot_factor == True):
-    a_vals = np.linspace(lowest_a, 1, 200)
-    D_vals = [D(a, lowest_a) for a in a_vals]
+if(mp.print_D_a == True):
+    a_vals = np.linspace(mp.a_i, 1, mp.num_steps)
+    D_vals = [D(a, mp.a_i) for a in a_vals]
 
     plt.plot(a_vals, D_vals, label="D(a)")
     plt.xlabel("a")
---
+++ parameters.py
@@ -0,0 +1,10 @@
+Omega_m = 0
+Omega_L = 1
+a_f = 1
+a_i = 0.01
+num_steps = 200
+
+print_D_a = True
+
+compare_case_1 = False
+compare_case_2 = False
