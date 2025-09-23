(BUG FIX) Add norm to dn directly and fix sample mean calculation BUG
Normalise the pdf as calculated by dn directly using the analytic form.
Note this will NOT generalise where the floor function is used on the PDF.

Also removed the second loop over mass for calculating the expectations --
this had made the sample expectation incorrect as it only used the PDF[]
list from the final part of the previous loop. It messes up the plot legends,
but surely this is something that can be fixed later.

Also:
- Removed print statements for the sample and analytic norms
- Just food for thought, I don't entirely understand why but it seems the
numerical expectation STILL needs to be normalised on the range, makes me
think I've perhaps done the norm a bit incorrectly here...
- Re-organised plot_dimensions=1 block

double-distribution.py
@@ -83,35 +83,29 @@ if pms.plot_dimension == 2:
     plt.close()
 
 elif pms.plot_dimension == 1:
-    mass_slices = np.array([0, 0.25, 0.5, 0.75, 1])
-    mass_slices *= (m_max - m_min) 
-    mass_slices += m_min
-
+    mass_slices = np.array([0, 0.25, 0.5, 0.75, 1]) * (m_max - m_min) + m_min
     for m in mass_slices:
         print("Starting PDF calculation for mass %.2E..." % m)
+        
         PDF = [func.dn(delta, m, pms.beta_dd) for delta in delta_l_vals]
-        plt.plot(delta_l_vals, PDF)
-
-    for m in mass_slices:
         analytic_norm, analytic_expectation = func.pdf_analytic_expectation(m, pms.beta_dd)
-        binned_analytic_expectation = max(delta for delta in delta_l_vals if delta <= analytic_expectation)
         sample_mean, sample_norm = func.pdf_sample_expectation(PDF, delta_l_vals)
 
-        print("Analytic norm: %.5E" % analytic_norm)
         print("Analytic expectation: %.5E" % analytic_expectation)
-        print("Sample norm: %.5E" % sample_norm)
         print("Sample expectation: %.5E" % sample_mean)
         print("---------")
 
-        plt.plot(binned_analytic_expectation, func.dn(binned_analytic_expectation, m, pms.beta_dd), 'b*', markersize=10)
+        plt.plot(delta_l_vals, PDF)
+        plt.plot(analytic_expectation, func.dn(analytic_expectation, m, pms.beta_dd), 'b*', markersize=10)
         plt.plot(sample_mean, func.dn(sample_mean, m, pms.beta_dd), 'r.', markersize=10)
     
     plt.xlabel(r"$\delta_l$")
     plt.ylabel(r"$P_n$")
-    plt.legend(["%.2E" % m for m in mass_slices])
     plt.title("PDF slices along mass")
     plt.savefig("plots/joint-pdf-slice.pdf")
     plt.grid(True)
+    #plt.legend(["%.2E" % m for m in mass_slices])
+
     plt.close()
 
 print("Printed density profile.")

util/functions.py
@@ -162,8 +162,10 @@ def dn(delta_l, m, beta, a:float = 1):
 
     random_walk = (pms.Omega_m * pms.rho_c / m ) * (np.exp(-(delta_l**2) / (2 * S(beta*m))) / (2 * np.pi * np.sqrt(S(beta*m))))
 
-    dn = random_walk * mass_removal 
-    
+    norm = delta_c_0(a) * (-S(beta * m) + S(m)) * np.exp(-delta_c_0(a)**2/2/S(m))
+    norm /= (S(m) * np.sqrt(-S(m)/(2 * S(beta * m)**2 * np.pi - 2 * S(beta * m) * S(m) * np.pi)))
+
+    dn = random_walk * mass_removal / norm
     if pms.enforce_positive_pdf == True:
         if dn < 0:
             return 0
@@ -195,11 +197,9 @@ def pdf_sample_expectation(pdf : list, delta_vals : NDArray):
     num_deltas = len(delta_vals)
     for i in range(num_deltas):
         sample_norm += pdf[i]
-    sample_norm /= num_deltas
 
     sample_mean = 0
     for i in range(num_deltas):
-        sample_mean += (pdf[i] / sample_norm) * delta_vals[i]
-    sample_mean /= num_deltas
+        sample_mean += pdf[i] * delta_vals[i] / sample_norm
 
     return sample_mean, sample_norm