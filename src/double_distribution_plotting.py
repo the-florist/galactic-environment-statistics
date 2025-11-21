c0e9a90a2d629066beda175b6a124d4a5ab775f5
Ericka Florio <eaf49@cam.ac.uk>
2025-11-21 20:49:03 +0200

    Move 2D plot to DoubleDistributionPlots class
    Add a new file, double_distribution_plotting, to store
    the DoubleDistributionPlots class, which performs all of
    the plotting routines.
    
    Moved the 2D plotting routine ONLY to a new member function
    of this class, plot_heatmap.
    
    Confirmed this returns the correct plot, when used in place
    of the old plotting routine.

---
+++ src/double_distribution_plotting.py
@@ -0,0 +1,18 @@
+"""
+    Filename: double-distribution.py
+    Author: Ericka Florio
+    Created: 11 Sept 2025
+    Description: Plots the joint double distribution for the number density of objects 
+            of mass m with local overdensity delta_l, 
+            as derived in Pavlidou and Fields 2005.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+import util.parameters as pms
+import util.functions as func
+import util.double_distribution_functions as ddfunc
+from util.Newton_method import NewtonsMethod
+from src.double_distribution_calculations import DoubleDistributionCalculations
